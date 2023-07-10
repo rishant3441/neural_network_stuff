0a77fd0063ee5ae2d04da30ed2220c7c122c7b27
Rishan Thangaraj <rishanthangaraj@gmail.com>
2023-07-10 02:49:17 -0400

    Add comments to NeuralNetwork.hpp

--- src/NeuralNetwork.hpp
+++ src/NeuralNetwork.hpp
@@ -31,17 +31,20 @@ namespace NN
         std::vector<aMatrix> outputs;
     };
 
+    // Layout in the form of [first layer # of neurons, 2nd layer, etc.]
+    // Currently only works with a 2 layer system (input and output)
     class BaseNetwork
     {
     public:
         BaseNetwork(int newLayout[], int layoutSize, const TrainingData& data, int epochs = 10*1000);
 
-        using aMatrix = Eigen::Matrix<float, Eigen::Dynamic, 1>;
-        using wMatrix = Eigen::Matrix<float, Eigen::Dynamic, Eigen::Dynamic>;
-        using bMatrix = Eigen::Matrix<float, Eigen::Dynamic, 1>;
+        using aMatrix = Eigen::Matrix<float, Eigen::Dynamic, 1>;                // Activation Matrix/Vector
+        using wMatrix = Eigen::Matrix<float, Eigen::Dynamic, Eigen::Dynamic>;   // Weight Matrix
+        using bMatrix = Eigen::Matrix<float, Eigen::Dynamic, 1>;                // Bias Matrix/Vector
 
         void Learn(float eps = 1e-1, float rate = 1e-1);
         void PrintResults();
+
     private:
         float cost(const wMatrix& weights, const bMatrix& bias, const TrainingData& data);
         void derivative_cost(wMatrix weights, bMatrix bias, float eps, const TrainingData& data, wMatrix* dw, bMatrix* b);
